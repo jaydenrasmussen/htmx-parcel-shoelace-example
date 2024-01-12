6d70f59de2c6d22319472213b7423ffd4dc66ee5
Jayden Rasmussen <jayden.rasmussen@gmail.com>
2024-01-12 12:11:57 -0800

    vue-version

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 import '@shoelace-style/shoelace/dist/shoelace.js';
 import '@shoelace-style/shoelace/dist/themes/light.css';
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+import { createApp, reactive } from 'petite-vue/dist/petite-vue.es.js';
 // Set the base path to the folder you copied Shoelace's assets to
 setBasePath('../node_modules/@shoelace-style/shoelace/dist');
+
+createApp({
+  store: reactive({
+    count: 0,
+    inc() {
+      this.count++
+    }
+  })
+}).mount('#app')
